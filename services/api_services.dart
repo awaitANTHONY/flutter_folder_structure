3027aa7941e44c19e191bdf6634de2cd49241dcd
ANTHONY <81618197+awaitANTHONY@users.noreply.github.com>
2023-06-06 21:32:33 +0600

    Update api_services.dart

--- services/api_services.dart
+++ services/api_services.dart
@@ -11,9 +11,9 @@ class ApiService {
   }
 
   static Future<http.Response> get(
-    String url, [
-    Map<String, String> headers = const {},
-  ]) async {
+    String url, {
+      Map<String, String> headers = const {},
+    }) async {
     var connectivityResult = await (Connectivity().checkConnectivity());
     if (connectivityResult != ConnectivityResult.none) {
       Uri uri = Uri.parse(url);
