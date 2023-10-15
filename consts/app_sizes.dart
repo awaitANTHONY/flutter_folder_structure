a34f7799abb29fd19d0a1344d03b053e190a5d6c
AnthonyRootdevs <85669051+AnthonyRootdevs@users.noreply.github.com>
2023-10-15 10:45:55 +0000

    Update app_sizes.dart

--- consts/app_sizes.dart
+++ consts/app_sizes.dart
@@ -7,20 +7,20 @@ var screenSize =
     currentOrientation == Orientation.portrait ? Get.height : Get.width;
 
 class AppSizes {
-  static final size12 = 1.5 * screenSize / 100;
-  static final size13 = 1.6 * screenSize / 100;
-  static final size14 = 1.8 * screenSize / 100;
-  static final size15 = 1.9 * screenSize / 100;
-  static final size16 = 2.0 * screenSize / 100;
-  static final size18 = 2.3 * screenSize / 100;
-  static final size20 = 2.5 * screenSize / 100;
-  static final size22 = 2.8 * screenSize / 100;
-  static final size24 = 3.1 * screenSize / 100;
-  static final size26 = 3.4 * screenSize / 100;
-  static final size28 = 3.7 * screenSize / 100;
-  static final size30 = 4.0 * screenSize / 100;
+  static final double size12 = 1.5 * screenSize / 100;
+  static final double size13 = 1.6 * screenSize / 100;
+  static final double size14 = 1.8 * screenSize / 100;
+  static final double size15 = 1.9 * screenSize / 100;
+  static final double size16 = 2.0 * screenSize / 100;
+  static final double size18 = 2.3 * screenSize / 100;
+  static final double size20 = 2.5 * screenSize / 100;
+  static final double size22 = 2.8 * screenSize / 100;
+  static final double size24 = 3.1 * screenSize / 100;
+  static final double size26 = 3.4 * screenSize / 100;
+  static final double size28 = 3.7 * screenSize / 100;
+  static final double size30 = 4.0 * screenSize / 100;
 
-  static newSize(percentage) {
+  static double newSize(percentage) {
     return percentage * screenSize / 100;
   }
 }
