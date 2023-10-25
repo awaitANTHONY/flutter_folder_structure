11fa2d2f50fe38daa52173260de8b7feb8d49619
ANTHONY <81618197+awaitANTHONY@users.noreply.github.com>
2023-10-25 23:01:00 +0600

    Update api_services.dart

--- services/api_services.dart
+++ services/api_services.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'package:chunked_uploader/chunked_uploader.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
-import '/utils/helpers.dart';
+import '/views/utils/helpers.dart';
 
 class ApiService {
   static final http.Client _client = http.Client();
@@ -12,8 +14,8 @@ class ApiService {
 
   static Future<http.Response> get(
     String url, {
-      Map<String, String> headers = const {},
-    }) async {
+    Map<String, String> headers = const {},
+  }) async {
     var connectivityResult = await (Connectivity().checkConnectivity());
     if (connectivityResult != ConnectivityResult.none) {
       Uri uri = Uri.parse(url);
@@ -78,6 +80,46 @@ class ApiService {
       var response = await http.Response.fromStream(streamedResponse);
 
       return response;
+    } else {
+      throw Exception('No internet connection please try again!');
+    }
+  }
+
+  static Future<Response> chunkFile(
+    String url,
+    String path, {
+    String field = 'file',
+    Map<String, String> headers = const {},
+    Map<String, String> body = const {},
+  }) async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult != ConnectivityResult.none) {
+      String fileName = path.split('/').last;
+      headers['Content-Type'] = 'multipart/form-data';
+      headers['Connection'] = 'Keep-Alive';
+
+      ChunkedUploader chunkedUploader = ChunkedUploader(
+        Dio(
+          BaseOptions(
+            baseUrl: url,
+            headers: headers,
+          ),
+        ),
+      );
+      Response? response = await chunkedUploader.uploadUsingFilePath(
+        fileName: fileName,
+        fileKey: field,
+        method: "POST",
+        filePath: path,
+        maxChunkSize: 500000,
+        path: url,
+        data: body,
+        onUploadProgress: (v) {
+          dd(v);
+        },
+      );
+
+      return response!;
     } else {
       throw Exception('No internet connection please try again!');
     }
