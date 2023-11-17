c506d3b4b07baded655c440450f1565ad6f47675
ANTHONY <81618197+awaitANTHONY@users.noreply.github.com>
2023-11-18 00:32:17 +0600

    Update consts.dart

--- consts/consts.dart
+++ consts/consts.dart
@@ -11,18 +11,16 @@ import '/controllers/setting_controller.dart';
 
 SettingController settingController = Get.find();
 
+extension SpaceXY on num {
+  SizedBox get spaceX => SizedBox(width: toDouble());
+  SizedBox get spaceY => SizedBox(height: toDouble());
+}
+
 extension EdgeInsetsTBXY on num {
   EdgeInsets get edgeT => EdgeInsets.only(top: toDouble());
   EdgeInsets get edgeB => EdgeInsets.only(bottom: toDouble());
-  EdgeInsets get edgeL => EdgeInsets.only(top: toDouble());
-  EdgeInsets get edgeR => EdgeInsets.only(bottom: toDouble());
+  EdgeInsets get edgeL => EdgeInsets.only(left: toDouble());
+  EdgeInsets get edgeR => EdgeInsets.only(right: toDouble());
   EdgeInsets get edgeX => EdgeInsets.symmetric(horizontal: toDouble());
   EdgeInsets get edgeY => EdgeInsets.symmetric(vertical: toDouble());
-}
-
-extension EdgeInsetsTBXY on double {
-  EdgeInsets get edgeT => EdgeInsets.only(top: this);
-  EdgeInsets get edgeB => EdgeInsets.only(bottom: this);
-  EdgeInsets get edgeX => EdgeInsets.symmetric(horizontal: this);
-  EdgeInsets get edgeY => EdgeInsets.symmetric(vertical: this);
 }
