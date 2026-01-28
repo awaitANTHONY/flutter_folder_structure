0148926bf0e3f714dc9c8c874d5d8bdc162d66e4
ANTHONY <81618197+awaitANTHONY@users.noreply.github.com>
2026-01-28 21:08:19 +0600

    Refactor ApiService for dynamic requests and errors
    Refactor ApiService to support dynamic request methods and improve error handling.

--- services/api_services.dart
+++ services/api_services.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
@@ -9,12 +8,17 @@ import 'package:chunked_uploader/chunked_uploader.dart';
 import '/utils/helpers.dart';
 import '/consts/consts.dart';
 
+export 'package:dio/dio.dart' show Response, DioException;
+
+enum Method { get, post, put, patch, delete }
+
 class ApiService {
   static final Dio _dio = Dio(
     BaseOptions(
-      connectTimeout: Duration(seconds: 30),
-      receiveTimeout: Duration(seconds: 30),
-      sendTimeout: Duration(seconds: 30),
+      connectTimeout: const Duration(seconds: 300),
+      receiveTimeout: const Duration(seconds: 300),
+      sendTimeout: const Duration(seconds: 300),
+      receiveDataWhenStatusError: true,
     ),
   );
 
@@ -22,92 +26,73 @@ class ApiService {
     dd('ApiService Init (Dio)');
   }
 
-  static Future<Response> get(
+  static Future<Response> request(
     String url, {
-    Map<String, String> headers = const {},
-    Map<String, dynamic> queryParams = const {},
-  }) async {
-    await _checkConnectivity();
-    headers = await _buildHeaders(headers);
-    return await _dio.get(
-      url,
-      options: Options(headers: headers),
-      queryParameters: queryParams,
-    );
-  }
-
-  static Future<Response> post(
-    String url, {
+    Map<String, dynamic> parameters = const {},
     Map<String, String> headers = const {},
     Map<String, dynamic> body = const {},
+    Method method = Method.get,
+    List<String>? filePaths,
+    List<String>? fileFieldNames,
+    void Function(dynamic percent)? progress,
+    bool isChunked = false,
+    bool showingErrors = true,
   }) async {
     await _checkConnectivity();
     headers = await _buildHeaders(headers);
-    dd(headers);
-    return await _dio.post(
-      url,
-      data: body,
-      options: Options(headers: headers),
-    );
+
+    if (filePaths != null && filePaths.isNotEmpty) {
+      if (isChunked) {
+        return _chunkFile(
+          url,
+          queryParameters: parameters,
+          filePaths.first,
+          field: fileFieldNames?.first ?? 'file',
+          headers: headers,
+          body: body.map((key, value) => MapEntry(key, value.toString())),
+          method: method,
+          progress: progress,
+        );
+      } else {
+        return _file(
+          url,
+          queryParameters: parameters,
+          paths: filePaths,
+          fields: fileFieldNames,
+          headers: headers,
+          body: body,
+          method: method,
+          progress: progress,
+        );
+      }
+    }
+
+    try {
+      return await _dio.request(
+        url,
+        options: Options(headers: headers, method: method.name.toUpperCase()),
+        queryParameters: parameters,
+        data: method != Method.get ? body : null,
+      );
+    } on DioException catch (e) {
+      if (showingErrors) {
+        _handleDioError(e);
+      }
+      rethrow;
+    }
   }
 
-  static Future<Response> patch(
+  static Future<Response> _file(
     String url, {
-    Map<String, String> headers = const {},
-    Map<String, dynamic> body = const {},
-  }) async {
-    await _checkConnectivity();
-    headers = await _buildHeaders(headers);
-    return await _dio.patch(
-      url,
-      data: body,
-      options: Options(headers: headers),
-    );
-  }
-
-  static Future<Response> put(
-    String url, {
-    Map<String, String> headers = const {},
-    Map<String, dynamic> body = const {},
-  }) async {
-    await _checkConnectivity();
-    headers = await _buildHeaders(headers);
-    return await _dio.put(
-      url,
-      data: body,
-      options: Options(headers: headers),
-    );
-  }
-
-  static Future<Response> delete(
-    String url, {
-    Map<String, String> headers = const {},
-    Map<String, dynamic> body = const {},
-  }) async {
-    await _checkConnectivity();
-    headers = await _buildHeaders(headers);
-    return await _dio.delete(
-      url,
-      data: body,
-      options: Options(headers: headers),
-    );
-  }
-
-  static Future<Response> file(
-    String url,
-    List<String>? paths, {
-    String field = 'file',
-    String method = "POST",
+    Map<String, dynamic> queryParameters = const {},
+    List<String>? paths,
+    List<String>? fields,
+    Method method = Method.get,
     Map<String, String> headers = const {},
     Map<String, dynamic> body = const {},
     void Function(dynamic percent)? progress,
   }) async {
     await _checkConnectivity();
-
-    // if (paths == null || paths.isEmpty) {
-    //   throw Exception('No file paths provided for upload!');
-    // }
-
     headers = await _buildMultipartHeaders(headers);
 
     List<MultipartFile> files = [];
@@ -124,37 +109,53 @@ class ApiService {
       }
     }
 
+    List<MapEntry<String, MultipartFile>> fileEntries = [];
+    for (int i = 0; i < files.length; i++) {
+      final fieldName = (fields != null && fields.length > i)
+          ? fields[i]
+          : (fields != null && fields.isNotEmpty)
+          ? fields.first
+          : 'file';
+      fileEntries.add(MapEntry(fieldName, files[i]));
+    }
+
     final formData = FormData.fromMap({
       ...body,
-      field: files.length == 1 ? files.first : files,
+      ...Map.fromEntries(fileEntries),
     });
 
-    final response = await _dio.request(
-      url,
-      data: formData,
-      options: Options(
-        method: method,
-        headers: headers,
-        contentType: 'multipart/form-data',
-      ),
-      onSendProgress: (sent, total) {
-        final progressPercent = (sent / total * 100).toStringAsFixed(0);
-        dd('Upload Progress: $progressPercent%');
-        if (progress != null) {
-          progress(progressPercent);
-        }
-      },
-    );
+    try {
+      final response = await _dio.request(
+        url,
+        queryParameters: queryParameters,
+        data: formData,
+        options: Options(
+          method: method.name.toUpperCase(),
+          headers: headers,
+          contentType: 'multipart/form-data',
+        ),
+        onSendProgress: (sent, total) {
+          final progressPercent = (sent / total * 100).toStringAsFixed(0);
+          dd('Upload Progress: $progressPercent%');
+          progress?.call(progressPercent);
+        },
+      );
 
-    return response;
+      return response;
+    } on DioException catch (e) {
+      _handleDioError(e);
+      rethrow;
+    }
   }
 
-  static Future<Response> chunkFile(
+  static Future<Response> _chunkFile(
     String url,
     String path, {
+    Map<String, dynamic> queryParameters = const {},
     String field = 'file',
     Map<String, String> headers = const {},
     Map<String, String> body = const {},
+    Method method = Method.post,
     void Function(dynamic percent)? progress,
   }) async {
     await _checkConnectivity();
@@ -172,22 +173,27 @@ class ApiService {
     }
 
     final chunkedUploader = ChunkedUploader(
-      Dio(BaseOptions(baseUrl: url, headers: headers)),
+      Dio(
+        BaseOptions(
+          baseUrl: url,
+          headers: headers,
+          method: method.name.toUpperCase(),
+          queryParameters: queryParameters,
+        ),
+      ),
     );
 
     final response = await chunkedUploader.uploadUsingFilePath(
       fileName: fileName,
       fileKey: field,
-      method: "POST",
+      method: Method.post.name.toUpperCase(),
       filePath: path,
       maxChunkSize: 500000,
       path: url,
       data: body,
       onUploadProgress: (progressPercent) {
         dd("Chunk Upload Progress: $progressPercent%");
-        if (progress != null) {
-          progress(progressPercent);
-        }
+        progress?.call(progressPercent);
       },
     );
 
@@ -197,7 +203,14 @@ class ApiService {
   static Future<void> _checkConnectivity() async {
     final connectivityResult = await Connectivity().checkConnectivity();
     if (connectivityResult.contains(ConnectivityResult.none)) {
-      throw Exception('No internet connection please try again!');
+      showToast(
+        'No internet connection. Please try again.',
+        type: ToastType.error,
+      );
+      throw DioException(
+        requestOptions: RequestOptions(path: ''),
+        message: 'No internet connection.',
+      );
     }
   }
 
@@ -231,7 +244,6 @@ class ApiService {
 
   static Future<String> generateUserAgent() async {
     final deviceInfo = DeviceInfoPlugin();
-
     if (Platform.isIOS) {
       var iosInfo = await deviceInfo.iosInfo;
       return 'Mozilla/5.0 (iPhone; CPU iPhone OS ${iosInfo.systemVersion.replaceAll('.', '_')} like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1';
@@ -241,5 +253,92 @@ class ApiService {
     } else {
       return 'Unknown User Agent';
     }
+  }
+
+  static void _handleDioError(DioException e) {
+    // return;
+    if (e.type == DioExceptionType.connectionTimeout ||
+        e.type == DioExceptionType.sendTimeout ||
+        e.type == DioExceptionType.receiveTimeout) {
+      showToast(
+        'Connection timed out. Please try again.',
+        type: ToastType.error,
+      );
+    } else if (e.type == DioExceptionType.connectionError) {
+      showToast(
+        'Failed to connect to server. Check your internet.',
+        type: ToastType.error,
+      );
+    } else if (e.response != null) {
+      final data = e.response?.data;
+
+      // dd('error response : $data', isShowLog: true, doCopy: true);
+
+      if (data is Map<String, dynamic>) {
+        showErrors(data);
+      } else {
+        showToast(
+          'Server error: ${e.response?.statusCode}',
+          type: ToastType.error,
+        );
+      }
+    } else {
+      // dd('${e.type.toString()} ${e.message}', isShowLog: true);
+      showToast(
+        'Unexpected error occurred. ${e.message}',
+        type: ToastType.error,
+      );
+    }
+  }
+}
+
+void showErrors(dynamic responseModel, {bool showMessageOnly = true}) {
+  if (responseModel == null) {
+    showToast('Unknown error occurred.', type: ToastType.error);
+    return;
+  }
+
+  final generalMessage =
+      responseModel['message'] ??
+      responseModel['errors'] ??
+      'Something went wrong.';
+
+  if (responseModel.containsKey('errors') &&
+      responseModel['errors'] is Map<String, dynamic> &&
+      responseModel['errors'].isNotEmpty) {
+    final Map<String, dynamic> errorsMap = Map<String, dynamic>.from(
+      responseModel['errors'],
+    );
+
+    final field = errorsMap.keys.first;
+    final messages = errorsMap[field];
+
+    if (messages is List) {
+      final msg = messages.first;
+      if (msg is Map) {
+        msg.forEach((key, value) {
+          final msg = value is String ? value : value[0].toString();
+          showToast("$key: $msg", type: ToastType.error);
+        });
+      } else {
+        showToast("$field: $msg", type: ToastType.error);
+      }
+    } else if (messages is Map) {
+      messages.forEach((subField, subMessages) {
+        if (subMessages is List) {
+          final List msg = subMessages.first;
+          showToast("$subField: $msg", type: ToastType.error);
+        } else {
+          showToast("$subField: $subMessages", type: ToastType.error);
+        }
+      });
+    } else {
+      showToast(
+        generalMessage == '' ? "$field: $messages" : generalMessage,
+        type: ToastType.error,
+      );
+    }
+  } else {
+    showToast(generalMessage, type: ToastType.error);
   }
 }
